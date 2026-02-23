89d72c0bb702504fea440b595a8122c7a50d651f
naziahasannidhi321-alt <naziahasannidhi321@gmail.com>
2026-02-23 15:17:29 +0000

    Left Mostlndex.C

---
+++ Left Mostlndex.C
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int n, key;
+
+    printf("Enter number of elements: 50");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input sorted array
+    printf("Enter %d sorted elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter value to search: 20");
+    scanf("%d", &key);
+
+    int low = 0, high = n - 1;
+    int mid;
+    int result = -1;   // Store leftmost index
+
+    // Binary Search for Leftmost index
+    while(low <= high) {
+
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            result = mid;      // Store index
+            high = mid - 1;    // Move left to find earlier occurrence
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;     // Search right half
+        }
+        else {
+            high = mid - 1;    // Search left half
+        }
+    }
+
+    if(result != -1)
+        printf("Left Most Index = %d", result);
+    else
+        printf("Element not found");
+
+    return 0;
+}
+🔎 কীভাবে কাজ করে?
